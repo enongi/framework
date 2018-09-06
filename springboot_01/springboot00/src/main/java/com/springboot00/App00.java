c9c40fe1b404e1db666457a076a139e1e0982833
lizhenzhong <lzzqqtxfh@163.com>
2018-09-06 11:11:48 +0800

    First Commit!

---
+++ springboot_01/springboot00/src/main/java/com/springboot00/App00.java
@@ -0,0 +1,14 @@
+package com.springboot00;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App00
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
